db17c5dacdeaa07a441e73a263760209f6016108
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2022-02-11 14:46:40 +0000

    sdi-40 add more hewell room mappings (#23)

---
+++ src/main/resources/db/migration/V1_6__room_mappings_hewell.sql
@@ -0,0 +1,4 @@
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open) values ('HEI-FAMILYTIME-HB1_6', 'HEI','VISITS-FAMILYTIME-HB1_6',true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open) values ('HEI-VISITS-H1_6LV', 'HEI','VISITS-VISITS-H1_6LV',true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open) values ('HEI-VISITS-HB8LV', 'HEI','VISITS-VISITS-HB8LV',true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open) values ('HEI-PACE_ROOM-PACE', 'HEI','VISITS-PACE_ROOM-PACE',true);
