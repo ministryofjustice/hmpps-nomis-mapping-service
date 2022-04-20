a1483d7f08dfef163a98c60abb49141f66e022a0
Peter Phillips <peter@greenthistle.co.uk>
2022-04-20 09:41:26 +0100

    SDI-171: 🔒️ Fix security issues by bumping versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4-beta"
-  kotlin("plugin.spring") version "1.6.20"
-  kotlin("plugin.jpa") version "1.6.20"
+  kotlin("plugin.spring") version "1.6.21"
+  kotlin("plugin.jpa") version "1.6.21"
   idea
 }
 
@@ -25,11 +25,11 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.1")
 
-  implementation("org.flywaydb:flyway-core:8.5.7")
-  implementation("com.vladmihalcea:hibernate-types-52:2.16.0")
+  implementation("org.flywaydb:flyway-core:8.5.8")
+  implementation("com.vladmihalcea:hibernate-types-52:2.16.1")
   runtimeOnly("io.r2dbc:r2dbc-postgresql")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
-  runtimeOnly("org.postgresql:postgresql:42.3.3")
+  runtimeOnly("org.postgresql:postgresql:42.3.4")
   implementation("io.opentelemetry:opentelemetry-api:1.13.0")
 
   implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.7")
@@ -44,10 +44,10 @@ dependencies {
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.4.0")
+  testImplementation("org.mockito:mockito-inline:4.5.0")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.32")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.17.0")
+  testImplementation("org.testcontainers:postgresql:1.17.1")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
 }
