d3fce985133f636169e064a6d453999678e76440
petergphillips <peter@greenthistle.co.uk>
2022-05-23 08:42:19 +0100

    SDI-211: ⬆️ Latest spring boot (#63)
    * SDI-211: ⬆️ Latest spring boot
    
    * SDI-211: 🐛 Specify postgres runtime version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.7-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.0-beta"
   kotlin("plugin.spring") version "1.6.21"
   kotlin("plugin.jpa") version "1.6.21"
   idea
@@ -27,7 +27,7 @@ dependencies {
 
   implementation("org.flywaydb:flyway-core:8.5.11")
   implementation("com.vladmihalcea:hibernate-types-52:2.16.2")
-  runtimeOnly("io.r2dbc:r2dbc-postgresql")
+  runtimeOnly("io.r2dbc:r2dbc-postgresql:0.8.12.RELEASE")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.3.5")
   implementation("io.opentelemetry:opentelemetry-api:1.14.0")
@@ -47,7 +47,7 @@ dependencies {
   testImplementation("org.mockito:mockito-inline:4.5.1")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.32")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.testcontainers:postgresql:1.17.1")
+  testImplementation("org.testcontainers:postgresql:1.17.2")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
 }
