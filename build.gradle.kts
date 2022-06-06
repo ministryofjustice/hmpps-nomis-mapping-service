bab3996c30c4514492bd8f0869a03d3632a8c14d
petergphillips <peter@greenthistle.co.uk>
2022-06-06 11:58:14 +0100

    SDI-218: ⬆️a Use latest plugin with suppressions (#69)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.0-beta-4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.1"
   kotlin("plugin.spring") version "1.6.21"
   kotlin("plugin.jpa") version "1.6.21"
   idea
@@ -20,14 +20,14 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.flywaydb:flyway-core:8.5.11")
+  implementation("org.flywaydb:flyway-core:8.5.12")
   implementation("com.vladmihalcea:hibernate-types-52:2.16.2")
   runtimeOnly("org.postgresql:postgresql:42.3.6")
   implementation("io.opentelemetry:opentelemetry-api:1.14.0")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.8")
-  implementation("org.springdoc:springdoc-openapi-security:1.6.8")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.9")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
 
@@ -37,7 +37,7 @@ dependencies {
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.5.1")
+  testImplementation("org.mockito:mockito-inline:4.6.1")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.0.33")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:postgresql:1.17.2")
