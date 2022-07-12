e3aa282a59575f6367cce04c1b49288473f1352d
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2022-07-12 11:17:01 +0100

    sdi-224 latest room mapping for Hewell (#83)
    Co-authored-by: lbennett-moj <lucy.bennett@digital .justice.gov.uk>

---
+++ src/main/resources/db/migration/V1_9__room_mappings_hewell.sql
@@ -0,0 +1,36 @@
+delete from room_id where prison_id = 'HEI';
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS', 'HEI', 'Visits Main Room', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS-SOC_VIS', 'HEI', 'Visits Main Room', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS-H1_6CLO', 'HEI', 'Visits Main Room', false);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS-H1_6LV', 'HEI', 'Legal Visit Room 6', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS-H1_6VL', 'HEI', 'Legal Visit Room 6', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS-HB7SOC', 'HEI', 'Visits Main Room', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS-HB7CLO', 'HEI', 'Visits Main Room', false);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS-HB7LV', 'HEI', 'Legal Visit Room 7', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS-HB8VA', 'HEI', 'Visits Main Room', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS-HB8LV', 'HEI', 'Legal Visit Room 8', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS1-6', 'HEI', 'Visits Main Room', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS1-6-H1_6SOC', 'HEI', 'Visits Main Room', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-VISITS1-6-H1_6CLO', 'HEI', 'Visits Main Room', false);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-PACE_ROOM', 'HEI', 'PACE Room', false);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-PACE_ROOM-PACE', 'HEI', 'PACE Room', false);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-FAMILYTIME', 'HEI', 'Visits Main Room', true);
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('HEI-FAMILYTIME-HB1_6', 'HEI', 'Visits Main Room', true);
