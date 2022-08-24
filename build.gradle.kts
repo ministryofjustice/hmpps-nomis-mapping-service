fc6702c9c9cf3e51d5582a3fb7d1365954f03f34
Peter Phillips <peter@greenthistle.co.uk>
2022-08-24 09:06:15 +0100

    SDI-348: ⬆️  Upgrade plugin to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.2"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
   idea
@@ -30,12 +30,12 @@ dependencies {
   implementation("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:r2dbc-postgresql:0.9.1.RELEASE")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
-  runtimeOnly("org.postgresql:postgresql:42.4.1")
-  implementation("io.opentelemetry:opentelemetry-api:1.16.0")
+  runtimeOnly("org.postgresql:postgresql:42.4.2")
+  implementation("io.opentelemetry:opentelemetry-api:1.17.0")
 
-  implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.9")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
-  implementation("org.springdoc:springdoc-openapi-security:1.6.9")
+  implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
+  implementation("org.springdoc:springdoc-openapi-security:1.6.11")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
 
@@ -43,8 +43,8 @@ dependencies {
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.6.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.1")
+  testImplementation("org.mockito:mockito-inline:4.7.0")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.2")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:postgresql:1.17.3")
   testImplementation("io.projectreactor:reactor-test")
