1e520b4170b4e33f71cfa39ccc886239a1a75450
petergphillips <peter@greenthistle.co.uk>
2022-09-12 15:00:03 +0100

    SDI-365: ⬆️  Upgrade plugin to latest to suppress snakeyaml vuln (#97)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.0"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
   idea
@@ -31,7 +31,7 @@ dependencies {
   runtimeOnly("org.postgresql:r2dbc-postgresql:0.9.2.RELEASE")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.5.0")
-  implementation("io.opentelemetry:opentelemetry-api:1.17.0")
+  implementation("io.opentelemetry:opentelemetry-api:1.18.0")
 
   implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.11")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
@@ -43,7 +43,7 @@ dependencies {
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.mockito:mockito-inline:4.7.0")
+  testImplementation("org.mockito:mockito-inline:4.8.0")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.2")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:postgresql:1.17.3")
