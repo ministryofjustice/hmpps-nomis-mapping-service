ef0683fc632c45d49f79ca11c98565258e2d81d5
Andy Marke <andy.marke@digital.justice.gov.uk>
2022-11-09 19:09:52 +0000

    SDI-446 added Bristol room mappings (#113)

---
+++ src/main/resources/db/migration/V1_11__room_mappings_bristol.sql
@@ -0,0 +1,27 @@
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('BLI-VISITS-SOC_VIS1', 'BLI', 'Visits Main Hall', true);
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('BLI-VISITS-CLO_VIS1', 'BLI', 'Visits Main Hall', false);
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('BLI-VISITS-CLO_VIS', 'BLI', 'Visits Main Hall', false);
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('BLI-VISITS-SOC_VIS', 'BLI', 'Visits Main Hall', true);
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('BLI-VISITS-SOC_VISPLUS', 'BLI', 'Visits Main Hall', true);
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('BLI-VISITS-OFF_VIS', 'BLI', 'Official Visit Room', true);
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('BLI-VISITS-PROB_VIS', 'BLI', 'Official Visit Room', true);
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('BLI-VISITS-VIDLNK', 'BLI', 'Video Link', true);
+
+insert into room_id (nomis_room_description, prison_id, vsip_id, is_open)
+values ('BLI-VISITS-HOSPITAL', 'BLI', 'Hospital', true);
