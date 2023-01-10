45067c443747f5269648687a401fcbdd2deea59b
Peter Phillips <peter@greenthistle.co.uk>
2023-01-10 16:20:23 +0000

    SDI-528: 🔒️ Upgrade plugin for snakeyaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.0.0-beta-3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.0.0-beta-4"
   kotlin("plugin.spring") version "1.8.0"
   kotlin("plugin.jpa") version "1.8.0"
   idea
@@ -38,7 +38,7 @@ dependencies {
   runtimeOnly("org.postgresql:r2dbc-postgresql:$r2dbcPostgresVersion")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.5.1")
-  implementation("io.opentelemetry:opentelemetry-api:1.21.0")
+  implementation("io.opentelemetry:opentelemetry-api:1.22.0")
 
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.0.2")
 
@@ -51,7 +51,7 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
   testImplementation("org.mockito:mockito-inline:4.11.0")
   testImplementation("javax.xml.bind:jaxb-api:2.3.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.9")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.10")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.testcontainers:postgresql:1.17.6")
   testImplementation("io.projectreactor:reactor-test")
