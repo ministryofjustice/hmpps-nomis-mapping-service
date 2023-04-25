ace404cc2a61467943bff3839b77b9a53418093c
steverendell <steve.rendell@syscon.net>
2023-04-25 09:10:19 +0100

    SDI-762 Add migration and label field to mapping

--- src/main/resources/db/migration/V1_18__appointment.sql
+++ src/main/resources/db/migration/V1_18__appointment.sql
@@ -1,2 +1,2 @@
-alter table appointment_mapping add column mapping_type varchar(20) not null;
+alter table appointment_mapping add column mapping_type varchar(20) not null default 'APPOINTMENT_CREATED';
 alter table appointment_mapping add column label        varchar(20);
