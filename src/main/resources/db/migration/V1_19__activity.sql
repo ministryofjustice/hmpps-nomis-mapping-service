7fef1094b3394fa47c76f55af5c60e10a1c31390
Mike Halma <mike.halma@digital.justice.gov.uk>
2023-06-07 10:17:25 +0100

    SDIT-837 Flyway uses migration of course

--- src/main/resources/db/migration/V1_19__activity.sql
+++ src/main/resources/db/migration/V1_19__activity.sql
@@ -2,7 +2,6 @@ create table activity_schedule_mapping (
     scheduled_instance_id          bigint       not null  PRIMARY KEY,
     nomis_course_schedule_id       bigint       not null,
     mapping_type                   varchar(20)  not null,
-    activity_schedule_id           bigint       not null,
     when_created                   timestamp with time zone not null default now(),
     constraint activity_schedule_mapping_crs_sch_id_unique unique(nomis_course_schedule_id)
 );
@@ -11,4 +10,3 @@ alter table activity_mapping add column when_created timestamp with time zone no
 
 create index activity_when_created_index on activity_mapping (when_created);
 create index activity_schedule_when_created_index on activity_schedule_mapping (when_created);
-create index activity_schedule_activity_schedule_id on activity_schedule_mapping (activity_schedule_id);
