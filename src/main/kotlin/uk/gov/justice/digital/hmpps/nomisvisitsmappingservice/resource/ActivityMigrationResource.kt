009dc0be63aca1995d67992bf1aa2c0112a228b9
Mike Halma <mike.halma@digital.justice.gov.uk>
2023-08-09 17:20:38 +0100

    SDIT-982 Add get activity migrations by migration ID endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/resource/ActivityMigrationResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/resource/ActivityMigrationResource.kt
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import jakarta.validation.Valid
 import org.springframework.dao.DuplicateKeyException
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
@@ -125,4 +128,35 @@ class ActivityMigrationResource(private val mappingService: ActivityMigrationSer
   )
   suspend fun getLatestMigratedMapping(): ActivityMigrationMappingDto =
     mappingService.getLatestMigrated()
+
+  @PreAuthorize("hasRole('ROLE_NOMIS_ACTIVITIES')")
+  @GetMapping("/mapping/activities/migration/migration-id/{migrationId}")
+  @Operation(
+    summary = "get paged mappings by migration id",
+    description = "Retrieve all activity migration mappings for the given migration id (identifies a single migration run). Results are paged.",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Mapping page returned",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ActivityMigrationMappingDto::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun getMigratedActivityMappings(
+    @PageableDefault pageRequest: Pageable,
+    @Schema(description = "Migration Id", example = "2020-03-24T12:00:00", required = true)
+    @PathVariable
+    migrationId: String,
+  ): Page<ActivityMigrationMappingDto> =
+    mappingService.getMappings(pageRequest = pageRequest, migrationId = migrationId)
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/service/ActivityMigrationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/service/ActivityMigrationService.kt
@@ -2,7 +2,13 @@ package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.service
 
 import com.microsoft.applicationinsights.TelemetryClient
 import jakarta.validation.ValidationException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.toList
 import org.slf4j.LoggerFactory
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.data.ActivityMigrationMappingDto
@@ -67,4 +73,24 @@ class ActivityMigrationService(
     activityMigrationMappingRepository.findFirstByOrderByWhenCreatedDesc()
       ?.let { ActivityMigrationMappingDto(it) }
       ?: throw NotFoundException("No migrated mapping found")
+
+  suspend fun getMappings(
+    pageRequest: Pageable,
+    migrationId: String,
+  ): Page<ActivityMigrationMappingDto> =
+    coroutineScope {
+      val activityMigrationMappings = async {
+        activityMigrationMappingRepository.findAllByLabelOrderByNomisCourseActivityIdAsc(label = migrationId, pageRequest)
+      }
+
+      val count = async {
+        activityMigrationMappingRepository.countAllByLabel(migrationId)
+      }
+
+      PageImpl(
+        activityMigrationMappings.await().toList().map { ActivityMigrationMappingDto(it) },
+        pageRequest,
+        count.await(),
+      )
+    }
 }
