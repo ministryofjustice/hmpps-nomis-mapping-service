4b50c1bd9693168d0ef23689785573601af2da02
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2023-08-10 15:37:55 +0100

    SDIT-982 Add get latest allocation migration mapping endpoint (#185)
    * SDIT-982 Add get latest allocation migration mapping endpoint
    
    * SDIT-982 Add get paged allocation migration mappings endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/jpa/repository/AllocationMigrationMappingRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/jpa/repository/AllocationMigrationMappingRepository.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.jpa.repository
 
+import kotlinx.coroutines.flow.Flow
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.jpa.AllocationMigrationMapping
@@ -7,4 +9,10 @@ import uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.jpa.AllocationMigr
 @Repository
 interface AllocationMigrationMappingRepository : CoroutineCrudRepository<AllocationMigrationMapping, Long> {
   suspend fun findOneByActivityAllocationId(activityAllocationId: Long): AllocationMigrationMapping?
+
+  suspend fun findFirstByOrderByWhenCreatedDesc(): AllocationMigrationMapping?
+
+  fun findAllByLabelOrderByNomisAllocationIdAsc(label: String, pageable: Pageable): Flow<AllocationMigrationMapping>
+
+  suspend fun countAllByLabel(label: String): Long
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/resource/AllocationMigrationResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/resource/AllocationMigrationResource.kt
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import jakarta.validation.Valid
 import org.springframework.dao.DuplicateKeyException
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
@@ -92,4 +95,66 @@ class AllocationMigrationResource(private val mappingService: AllocationMigratio
   suspend fun getMapping(
     @Schema(description = "Nomis allocation Id", example = "12345", required = true) @PathVariable nomisAllocationId: Long,
   ): AllocationMigrationMappingDto = mappingService.getMapping(nomisAllocationId)
+
+  @PreAuthorize("hasRole('ROLE_NOMIS_ACTIVITIES')")
+  @GetMapping("/mapping/allocations/migrated/latest")
+  @Operation(
+    summary = "get the latest mapping for an allocation migration",
+    description = "Requires role NOMIS_ACTIVITIES",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Mapping Information Returned",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = AllocationMigrationMappingDto::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "No mappings found at all for any migration",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun getLatestMigratedMapping(): AllocationMigrationMappingDto =
+    mappingService.getLatestMigrated()
+
+  @PreAuthorize("hasRole('ROLE_NOMIS_ACTIVITIES')")
+  @GetMapping("/mapping/allocations/migration/migration-id/{migrationId}")
+  @Operation(
+    summary = "get paged mappings by migration id",
+    description = "Retrieve all allocation migration mappings for the given migration id (identifies a single migration run). Results are paged.",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Mapping page returned",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = AllocationMigrationMappingDto::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun getMigratedAllocationMappings(
+    @PageableDefault pageRequest: Pageable,
+    @Schema(description = "Migration Id", example = "2020-03-24T12:00:00", required = true)
+    @PathVariable
+    migrationId: String,
+  ): Page<AllocationMigrationMappingDto> =
+    mappingService.getMappings(pageRequest = pageRequest, migrationId = migrationId)
 }
