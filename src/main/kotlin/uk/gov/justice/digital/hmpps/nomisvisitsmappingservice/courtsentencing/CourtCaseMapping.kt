a5b2fc4f1b0f1c6bc6c5bd4720dd99eb43c13b82
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2024-02-21 08:46:52 +0000

    sdit-1514 add mappings for Create Court Case (#247)
    Co-authored-by: lbennett-moj <lucy.bennett@digital .justice.gov.uk>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/courtsentencing/CourtCaseMapping.kt
@@ -0,0 +1,50 @@
+package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.courtsentencing
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Transient
+import org.springframework.data.domain.Persistable
+import java.time.LocalDateTime
+
+data class CourtCaseMapping(
+
+  @Id
+  val dpsCourtCaseId: String,
+
+  val nomisCourtCaseId: Long,
+
+  /**
+   * ISO timestamp of batch job if a migration
+   */
+  val label: String? = null,
+
+  val mappingType: CourtCaseMappingType,
+
+  @Transient
+  @Value("false")
+  val new: Boolean = true,
+
+  val whenCreated: LocalDateTime? = null,
+
+) : Persistable<String> {
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is CourtCaseMapping) return false
+
+    return dpsCourtCaseId == other.dpsCourtCaseId
+  }
+
+  override fun hashCode(): Int {
+    return dpsCourtCaseId.hashCode()
+  }
+
+  override fun isNew(): Boolean = new
+
+  override fun getId(): String = dpsCourtCaseId
+}
+
+enum class CourtCaseMappingType {
+  MIGRATED,
+  DPS_CREATED,
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/courtsentencing/CourtSentencingMappingResource.kt
@@ -0,0 +1,117 @@
+package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.courtsentencing
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import jakarta.validation.Valid
+import org.springframework.dao.DuplicateKeyException
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.config.DuplicateMappingErrorResponse
+import uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.config.DuplicateMappingException
+import uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.config.ErrorResponse
+
+@RestController
+@Validated
+@PreAuthorize("hasRole('NOMIS_COURT_SENTENCING')")
+@RequestMapping("/mapping/court-sentencing", produces = [MediaType.APPLICATION_JSON_VALUE])
+class CourtSentencingMappingResource(private val mappingService: CourtSentencingMappingService) {
+  @GetMapping("/court-cases/dps-court-case-id/{courtCaseId}")
+  @Operation(
+    summary = "get court case mapping",
+    description = "Retrieves a mapping by DPS id. Requires role NOMIS_COURT_SENTENCING",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Mapping Information Returned",
+        content = [
+          Content(mediaType = "application/json", schema = Schema(implementation = CourtCaseMappingDto::class)),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Id does not exist in mapping table",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun getMappingByNomisId(
+    @Schema(description = "DPS court case id", example = "D123", required = true)
+    @PathVariable
+    courtCaseId: String,
+  ): CourtCaseMappingDto = mappingService.getCourtCaseMappingByDpsId(
+    courtCaseId = courtCaseId,
+  )
+
+  @PostMapping("/court-cases")
+  @ResponseStatus(HttpStatus.CREATED)
+  @Operation(
+    summary = "Creates a new court case mapping",
+    description = "Creates a mapping between nomis Court Case ID and DPS Court Case ID. Requires ROLE_NOMIS_COURT_SENTENCING",
+    requestBody = io.swagger.v3.oas.annotations.parameters.RequestBody(
+      content = [Content(mediaType = "application/json", schema = Schema(implementation = CourtCaseMappingDto::class))],
+    ),
+    responses = [
+      ApiResponse(responseCode = "201", description = "Mapping created"),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Access forbidden for this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "409",
+        description = "Indicates a duplicate mapping has been rejected. If Error code = 1409 the body will return a DuplicateErrorResponse",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = DuplicateMappingErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  suspend fun createMapping(
+    @RequestBody @Valid
+    mapping: CourtCaseMappingDto,
+  ) =
+    try {
+      mappingService.createMapping(mapping)
+    } catch (e: DuplicateKeyException) {
+      throw DuplicateMappingException(
+        messageIn = "Court Case mapping already exists",
+        duplicate = mapping,
+        existing = getExistingMappingSimilarTo(mapping),
+        cause = e,
+      )
+    }
+
+  private suspend fun getExistingMappingSimilarTo(mapping: CourtCaseMappingDto) = runCatching {
+    mappingService.getCourtCaseMappingByNomisId(
+      courtCaseId = mapping.nomisCourtCaseId,
+    )
+  }.getOrElse {
+    mappingService.getCourtCaseMappingByDpsId(
+      courtCaseId = mapping.dpsCourtCaseId,
+    )
+  }
+}
