c4eeb0a3005086bb13933b3739f90ab0c1b75e2d
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2024-02-28 14:44:56 +0000

    sdit-1524 add mappings for hierarchical Create Court case mapping - C… (#254)
    * sdit-1524 add mappings for hierarchical Create Court case mapping - Charges
    
    * sdit-1524 add mappings for hierarchical Create Court case mapping - Charges
    
    ---------
    
    Co-authored-by: lbennett-moj <lucy.bennett@digital .justice.gov.uk>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/courtsentencing/CourtChargeMapping.kt
@@ -0,0 +1,50 @@
+package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.courtsentencing
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Transient
+import org.springframework.data.domain.Persistable
+import java.time.LocalDateTime
+
+data class CourtChargeMapping(
+
+  @Id
+  val dpsCourtChargeId: String,
+
+  val nomisCourtChargeId: Long,
+
+  /**
+   * ISO timestamp of batch job if a migration
+   */
+  val label: String? = null,
+
+  val mappingType: CourtChargeMappingType,
+
+  @Transient
+  @Value("false")
+  val new: Boolean = true,
+
+  val whenCreated: LocalDateTime? = null,
+
+) : Persistable<String> {
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is CourtChargeMapping) return false
+
+    return dpsCourtChargeId == other.dpsCourtChargeId
+  }
+
+  override fun hashCode(): Int {
+    return dpsCourtChargeId.hashCode()
+  }
+
+  override fun isNew(): Boolean = new
+
+  override fun getId(): String = dpsCourtChargeId
+}
+
+enum class CourtChargeMappingType {
+  MIGRATED,
+  DPS_CREATED,
+}
