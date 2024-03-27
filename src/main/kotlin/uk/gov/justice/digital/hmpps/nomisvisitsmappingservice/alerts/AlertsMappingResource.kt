3d140fe33eefecc3cc77b8bc9623bcef93ea4947
Andy Marke <andy.marke@digital.justice.gov.uk>
2024-03-27 11:13:57 +0000

    SDIT-1615 Add Alerts get by migrationId

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/alerts/AlertsMappingResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/alerts/AlertsMappingResource.kt
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import jakarta.validation.Valid
 import org.springframework.dao.DuplicateKeyException
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
@@ -185,6 +188,35 @@ class AlertsMappingResource(private val mappingService: AlertMappingService) {
     ],
   )
   suspend fun deleteAllMappings() = mappingService.deleteAllMappings()
+
+  @GetMapping("/migration-id/{migrationId}")
+  @Operation(
+    summary = "Get paged mappings by migration id",
+    description = "Retrieve all mappings of type 'MIGRATED' for the given migration id (identifies a single migration run). Results are paged.",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Mapping page returned",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun getMappingsByMigrationId(
+    @PageableDefault pageRequest: Pageable,
+    @Schema(description = "Migration Id", example = "2020-03-24T12:00:00", required = true)
+    @PathVariable
+    migrationId: String,
+  ): Page<AlertMappingDto> =
+    mappingService.getByMigrationId(pageRequest = pageRequest, migrationId = migrationId)
 
   private suspend fun getExistingMappingSimilarTo(mapping: AlertMappingDto) = runCatching {
     mappingService.getMappingByNomisId(
