746d47e2942af0b8bc47bd52d518f1e4d3ada413
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-04-29 17:55:00 +0100

    SDIT-1700: 🩺 Upgrade to reduce health false positive alerts (#285)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.4")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
