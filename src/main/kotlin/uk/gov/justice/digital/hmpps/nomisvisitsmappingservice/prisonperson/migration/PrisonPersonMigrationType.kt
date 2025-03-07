98009ee23fa6857040b9993a5be5491d26ca9a48
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-03-07 16:20:58 +0000

    SDIT-2518: 🔥 Remove prison person

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/prisonperson/migration/PrisonPersonMigrationType.kt
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.prisonperson.migration
-
-enum class PrisonPersonMigrationType {
-  PHYSICAL_ATTRIBUTES,
-  PROFILE_DETAILS_PHYSICAL_ATTRIBUTES,
-}
