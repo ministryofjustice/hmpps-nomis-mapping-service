94a828664b10b18edf4bdeb50bc7e786d877061a
andymarke <andy.marke@digital.justice.gov.uk>
2025-06-11 12:02:08 +0100

    SDIT-2817 court_appearance_recall_mapping
    Get mappings by DPS id

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/courtsentencing/CourtSentencingMappingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/courtsentencing/CourtSentencingMappingService.kt
@@ -462,6 +462,7 @@ class CourtSentencingMappingService(
     courtCaseMappingRepository.deleteAll()
     courtCasePrisonerMappingRepository.deleteAll()
     courtAppearanceMappingRepository.deleteAll()
+    courtAppearanceRecallMappingRepository.deleteAll()
     courtChargeMappingRepository.deleteAll()
     sentenceMappingRepository.deleteAll()
     sentenceTermMappingRepository.deleteAll()
