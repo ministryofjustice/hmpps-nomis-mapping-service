2160dc6b2f9fb57a3b5ebb50b28bb46a1493bf05
andymarke <andy.marke@digital.justice.gov.uk>
2025-06-11 11:24:13 +0100

    SDIT-2817 court_appearance_recall_mapping

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/courtsentencing/CourtAppearanceRecallMappingDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/courtsentencing/CourtAppearanceRecallMappingDto.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.courtsentencing
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Size
 import java.time.LocalDateTime
 
@@ -27,4 +28,27 @@ data class CourtAppearanceRecallMappingDto(
 
   @Schema(description = "Date-time the mapping was created")
   val whenCreated: LocalDateTime? = null,
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "Court appearance recall mappings")
+data class CourtAppearanceRecallMappingsDto(
+
+  @Schema(description = "List of NOMIS court appearance ids", example = "[123456, 789012]")
+  @field:NotEmpty(message = "NOMIS court appearance ids must be supplied")
+  val nomisCourtAppearanceIds: List<Long>,
+
+  @Schema(description = "DPS recall id", example = "123456")
+  @field:NotEmpty(message = "DPS recall id must be supplied")
+  val dpsRecallId: String,
+
+  @Schema(description = "Label (a timestamp for migrated ids)")
+  @field:Size(max = 20)
+  val label: String? = null,
+
+  @Schema(
+    description = "Mapping type",
+    defaultValue = "DPS_CREATED",
+  )
+  val mappingType: CourtAppearanceRecallMappingType? = null,
 )
