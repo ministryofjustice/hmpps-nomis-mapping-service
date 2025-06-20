2a2bf3dc962366b10ee04340b8cf141ddfecad5f
karenmillermoj <karen.miller3@justice.gov.uk>
2025-06-20 09:56:32 +0100

    SDIT-2829 Regularly clear visit balance adjustment mapping table

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/timed/ClearVisitBalanceAdjustmentServiceTest.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.timed
+
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.check
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.visitbalances.VisitBalanceAdjustmentMappingRepository
+import uk.gov.justice.digital.hmpps.timed.ClearVisitBalanceAdjustmentService
+import java.time.LocalDate
+import java.time.LocalTime
+
+class ClearVisitBalanceAdjustmentServiceTest {
+  val repository: VisitBalanceAdjustmentMappingRepository = mock()
+  val service = ClearVisitBalanceAdjustmentService(repository)
+
+  @Test
+  fun clearVisitBalanceAdjustments() = runBlocking {
+    service.clearExpiredEntries(4)
+    verify(repository).deleteByWhenCreatedBefore(
+      check {
+        assertThat(it.toLocalDate()).isEqualTo(LocalDate.now().minusDays(4))
+        assertThat(it.toLocalTime()).isEqualTo(LocalTime.MIDNIGHT)
+      },
+    )
+  }
+}
