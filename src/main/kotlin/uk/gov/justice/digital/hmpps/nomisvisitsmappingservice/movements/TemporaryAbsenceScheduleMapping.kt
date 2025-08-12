e7e15fe3f7954d8b1e5b569c327a0d450aff035e
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-08-12 14:38:45 +0100

    SDIT-2873 add temporary absence schedule and movement mapping tables

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/movements/TemporaryAbsenceScheduleMapping.kt
@@ -0,0 +1,48 @@
+package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.movements
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Transient
+import org.springframework.data.domain.Persistable
+import java.time.LocalDateTime
+import java.util.*
+
+data class TemporaryAbsenceScheduleMapping(
+
+  @Id
+  val dpsScheduleId: UUID,
+
+  val nomisScheduleId: Long,
+
+  val offenderNo: String,
+
+  val bookingId: Long,
+
+  /**
+   * ISO timestamp of batch job if a migration
+   */
+  val label: String? = null,
+
+  val mappingType: MovementMappingType,
+
+  @Transient
+  @Value("false")
+  val new: Boolean = true,
+
+  val whenCreated: LocalDateTime? = null,
+
+) : Persistable<UUID> {
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other !is TemporaryAbsenceScheduleMapping) return false
+
+    return dpsScheduleId != other.dpsScheduleId
+  }
+
+  override fun hashCode(): Int = dpsScheduleId.hashCode()
+
+  override fun isNew(): Boolean = new
+
+  override fun getId(): UUID = dpsScheduleId
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/movements/TemporaryAbsenceScheduleRepository.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.movements
+
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface TemporaryAbsenceScheduleRepository : CoroutineCrudRepository<TemporaryAbsenceScheduleMapping, UUID> {
+  suspend fun findByNomisScheduleId(nomisScheduleId: Long): TemporaryAbsenceScheduleMapping?
+}
