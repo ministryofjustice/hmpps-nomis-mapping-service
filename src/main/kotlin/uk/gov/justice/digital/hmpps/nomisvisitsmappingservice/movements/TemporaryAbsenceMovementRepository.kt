3f2ce68749c707dd88bb91f2f3fa83da3853bd38
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-08-12 14:55:19 +0100

    SDIT-2873 add temporary absence schedule and movement mapping tables (#527)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisvisitsmappingservice/movements/TemporaryAbsenceMovementRepository.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.digital.hmpps.nomisvisitsmappingservice.movements
+
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface TemporaryAbsenceMovementRepository : CoroutineCrudRepository<TemporaryAbsenceMovementMapping, UUID> {
+  suspend fun findByNomisBookingIdAndNomisMovementSeq(nomisBookingId: Long, nomisMovementSeq: Int): TemporaryAbsenceMovementMapping?
+}
---
+++ src/main/resources/db/migration/V1_107__temporary_absence_movement.sql
@@ -0,0 +1,14 @@
+create table temporary_absence_movement_mapping
+(
+    dps_movement_id         uuid                     not null PRIMARY KEY,
+    nomis_booking_id        bigint                   not null,
+    nomis_movement_seq      int                      not null,
+    offender_no             varchar(10)              not null,
+    when_created            timestamp with time zone not null default now(),
+    label                   varchar(20),
+    mapping_type            varchar(20)              not null,
+    constraint temporary_absence_movement_mapping_nomis_id_unique unique (nomis_booking_id, nomis_movement_seq)
+);
+create index temporary_absence_movement_mapping_when_created_index on temporary_absence_movement_mapping (when_created);
+create index temporary_absence_movement_mapping_label_index on temporary_absence_movement_mapping (label);
+create index temporary_absence_movement_mapping_offender_no on temporary_absence_movement_mapping (offender_no);
