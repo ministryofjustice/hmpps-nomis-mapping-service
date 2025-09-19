fdeb0e8b27885a1173521dabe15f3d5b4b224346
karenmillermoj <karen.miller3@justice.gov.uk>
2025-09-19 16:41:04 +0100

    SDIT-2830 Rebase

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/visitbalances/VisitBalanceAdjustmentMappingRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/visitbalances/VisitBalanceAdjustmentMappingRepository.kt
@@ -2,9 +2,11 @@ package uk.gov.justice.digital.hmpps.nomismappingservice.visitbalances
 
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 @Repository
 interface VisitBalanceAdjustmentMappingRepository : CoroutineCrudRepository<VisitBalanceAdjustmentMapping, String> {
   suspend fun findOneByNomisId(nomisVisitBalanceAdjustmentId: Long): VisitBalanceAdjustmentMapping?
   suspend fun findAllByDpsIdOrderByNomisIdDesc(dpsId: String): List<VisitBalanceAdjustmentMapping>
+  suspend fun deleteByWhenCreatedBefore(toDate: LocalDateTime)
 }
