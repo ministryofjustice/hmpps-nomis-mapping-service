eca15bc08db679d6cfc5e2a25f3983cff5d8fbf4
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2025-09-24 17:21:42 +0100

    SDIT-2830 Ensure scheduling switched on for scheduled tasks (#559)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/NomisMappingService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/NomisMappingService.kt
@@ -2,7 +2,9 @@ package uk.gov.justice.digital.hmpps.nomismappingservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication()
 class NomisMappingService
 
