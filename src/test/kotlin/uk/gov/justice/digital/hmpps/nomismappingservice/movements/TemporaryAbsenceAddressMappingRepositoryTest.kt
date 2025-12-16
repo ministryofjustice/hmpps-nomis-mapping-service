6f61f32955dcca5dc79ba0f5d86add006f991e86
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-12-16 11:52:25 +0000

    SDIT-2868 Respect null columns as unique values to prevent duplicate addresses (#611)

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/movements/TemporaryAbsenceAddressMappingRepositoryTest.kt
@@ -0,0 +1,55 @@
+package uk.gov.justice.digital.hmpps.nomismappingservice.movements
+
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.nomismappingservice.helper.TestBase
+import uk.gov.justice.hmpps.test.kotlin.auth.WithMockAuthUser
+
+@DataR2dbcTest
+@ActiveProfiles("test")
+@WithMockAuthUser
+class TemporaryAbsenceAddressMappingRepositoryTest(
+  @Autowired private val repository: TemporaryAbsenceAddressRepository,
+) : TestBase() {
+
+  private val nomisAddressId = 12345L
+  private val nomisAddressOwnerClass = "CORP"
+  private val nomisOffenderNo = null
+  private val dpsAddressText = "DPS address text"
+  private val dpsUprn = null
+
+  @AfterEach
+  fun tearDown() = runTest {
+    repository.deleteAll()
+  }
+
+  @Test
+  fun `should prevent duplicate address mappings with null columns`() = runTest {
+    repository.save(
+      TemporaryAbsenceAddressMapping(
+        nomisAddressId = nomisAddressId,
+        nomisAddressOwnerClass = nomisAddressOwnerClass,
+        nomisOffenderNo = nomisOffenderNo,
+        dpsAddressText = dpsAddressText,
+        dpsUprn = dpsUprn,
+      ),
+    )
+
+    assertThrows<Exception> {
+      repository.save(
+        TemporaryAbsenceAddressMapping(
+          nomisAddressId = nomisAddressId,
+          nomisAddressOwnerClass = nomisAddressOwnerClass,
+          nomisOffenderNo = nomisOffenderNo,
+          dpsAddressText = dpsAddressText,
+          dpsUprn = dpsUprn,
+        ),
+      )
+    }
+  }
+}
