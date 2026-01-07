bdbb44158ebe188a59243195f6632b7a709a8e4c
Mike Halma <mike.halma@digital.justice.gov.uk>
2026-01-07 15:03:25 +0000

    SDIT-2868 Increase address text column size to handle new address format

---
+++ src/main/resources/db/migration/V1_127__temporary_absence_addresses.sql
@@ -0,0 +1,3 @@
+alter table temporary_absence_schedule_mapping alter column dps_address_text type varchar(500);
+alter table temporary_absence_movement_mapping alter column dps_address_text type varchar(500);
+alter table temporary_absence_address_mapping alter column dps_address_text type varchar(500);
