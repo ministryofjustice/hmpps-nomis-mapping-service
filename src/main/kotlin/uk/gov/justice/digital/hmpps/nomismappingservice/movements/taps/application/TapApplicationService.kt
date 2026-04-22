bc36aa05438b2fb1db2a2405a5a4caf01eb53ada
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-04-22 08:59:43 +0100

    SDIT-2832 Add new tap application API (#693)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/movements/taps/application/TapApplicationService.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.nomismappingservice.movements.taps.application
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.nomismappingservice.service.NotFoundException
+import java.util.UUID
+
+@Service
+class TapApplicationService(
+  private val applicationRepository: TapApplicationRepository,
+) {
+
+  @Transactional
+  suspend fun createApplicationMapping(mappingDto: TapApplicationMappingDto) {
+    applicationRepository.save(mappingDto.toMapping())
+  }
+
+  suspend fun getApplicationMappingByNomisId(nomisApplicationId: Long) = applicationRepository.findByNomisApplicationId(nomisApplicationId)
+    ?.toMappingDto()
+    ?: throw NotFoundException("Mapping for NOMIS application id $nomisApplicationId not found")
+
+  suspend fun getApplicationMappingByDpsId(dpsApplicationId: UUID) = applicationRepository.findById(dpsApplicationId)
+    ?.toMappingDto()
+    ?: throw NotFoundException("Mapping for DPS application id $dpsApplicationId not found")
+
+  @Transactional
+  suspend fun deleteApplicationMappingByNomisId(nomisApplicationId: Long) = applicationRepository.deleteByNomisApplicationId(nomisApplicationId)
+}
+
+fun TapApplicationMappingDto.toMapping(): TapApplicationMapping = TapApplicationMapping(
+  dpsAuthorisationId,
+  nomisApplicationId,
+  prisonerNumber,
+  bookingId,
+  mappingType = mappingType,
+)
+
+fun TapApplicationMapping.toMappingDto(): TapApplicationMappingDto = TapApplicationMappingDto(
+  offenderNo,
+  bookingId,
+  nomisApplicationId,
+  dpsAuthorisationId,
+  mappingType = mappingType,
+)
