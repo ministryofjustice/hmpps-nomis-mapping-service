fb1c14c6c4e70f686d115dadb94cfd57d374ef87
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-05-12 11:54:41 +0100

    SDIT-3732 Design the API for migrating court scheduler mappings (#712)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/movements/court/migration/CourtSchedulerMigrationResource.kt
@@ -0,0 +1,51 @@
+package uk.gov.justice.digital.hmpps.nomismappingservice.movements.court.migration
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+
+@RestController
+@Validated
+@PreAuthorize("hasRole('NOMIS_MAPPING_API__SYNCHRONISATION__RW')")
+@RequestMapping("/mapping/court", produces = [MediaType.APPLICATION_JSON_VALUE])
+class CourtSchedulerMigrationResource(
+  private val service: CourtSchedulerMigrationService,
+) {
+
+  @PutMapping("/migrate")
+  @ResponseStatus(HttpStatus.CREATED)
+  @Operation(
+    summary = "Creates all mappings for prisoner court movements which are all migrated at the same time",
+    description = "Creates mappings for prisoner court movements. Requires ROLE_NOMIS_MAPPING_API__SYNCHRONISATION__RW",
+    requestBody = io.swagger.v3.oas.annotations.parameters.RequestBody(
+      content = [Content(mediaType = "application/json", schema = Schema(implementation = CourtSchedulerPrisonerMappingsDto::class))],
+    ),
+    responses = [
+      ApiResponse(responseCode = "201", description = "Mappings created"),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Access forbidden for this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun createPrisonerCourtSchedulerMappings(
+    @RequestBody mappings: CourtSchedulerPrisonerMappingsDto,
+  ) = service.createMigrationMappings(mappings)
+}
