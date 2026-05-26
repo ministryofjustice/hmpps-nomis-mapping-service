3650d66846bbc1eb5bb511d0e023dfe0df47118e
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-05-26 08:41:10 +0100

    SDIT-3827: ✅ Fix test to inject build properties and not be dependent on date built (#725)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/resource/OpenApiDocsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/resource/OpenApiDocsTest.kt
@@ -4,16 +4,18 @@ import io.swagger.v3.parser.OpenAPIV3Parser
 import net.minidev.json.JSONArray
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.info.BuildProperties
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient
 import org.springframework.http.MediaType
 import uk.gov.justice.digital.hmpps.nomismappingservice.integration.IntegrationTestBase
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import kotlin.text.get
 
-@AutoConfigureWebTestClient(timeout = "20000")
-class OpenApiDocsTest : IntegrationTestBase() {
+@AutoConfigureWebTestClient(timeout = "PT60S")
+class OpenApiDocsTest(
+  @Autowired private val buildProperties: BuildProperties,
+) : IntegrationTestBase() {
   @LocalServerPort
   private var port: Int = 0
 
@@ -60,9 +62,7 @@ class OpenApiDocsTest : IntegrationTestBase() {
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
       .expectStatus().isOk
-      .expectBody().jsonPath("info.version").value<String> {
-        assertThat(it).startsWith(DateTimeFormatter.ISO_DATE.format(LocalDate.now()))
-      }
+      .expectBody().jsonPath("info.version").isEqualTo(buildProperties.version)
   }
 
   @Test
