e301a231011508939398a86412416c44eb60d6fa
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2026-06-23 14:01:31 +0100

    SDIT-3845 Fix update of nomis booking ID (#747)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/movements/court/schedule/CourtScheduleService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/movements/court/schedule/CourtScheduleService.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.nomismappingservice.movements.court.schedule
 
+import org.springframework.dao.DuplicateKeyException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.nomismappingservice.config.DuplicateMappingException
 import uk.gov.justice.digital.hmpps.nomismappingservice.service.NotFoundException
 import java.util.UUID
 
@@ -16,8 +18,11 @@ class CourtScheduleService(
   }
 
   @Transactional
-  suspend fun updateNomisEventId(dpsCourtAppearanceId: UUID, newNomisEventId: Long) = scheduleRepository.findByDpsCourtAppearanceId(dpsCourtAppearanceId)!!
-    .apply { nomisEventId = newNomisEventId }
+  suspend fun updateNomisEventId(dpsCourtAppearanceId: UUID, newNomisEventId: Long, newBookingId: Long) = scheduleRepository.findByDpsCourtAppearanceId(dpsCourtAppearanceId)!!
+    .apply {
+      nomisEventId = newNomisEventId
+      bookingId = newBookingId
+    }
     .also { scheduleRepository.save(it) }
 
   suspend fun getScheduleMappingByNomisId(nomisEventId: Long) = scheduleRepository.findByNomisEventId(nomisEventId)
@@ -33,6 +38,38 @@ class CourtScheduleService(
 
   @Transactional
   suspend fun deleteScheduleMappingByDpsId(dpsId: UUID) = scheduleRepository.deleteByDpsCourtAppearanceId(dpsId)
+
+  suspend fun getExistingCourtScheduleMappingSimilarTo(mapping: CourtScheduleMappingDto) = runCatching {
+    getScheduleMappingByNomisId(mapping.nomisEventId)
+  }
+    .getOrElse {
+      getScheduleMappingByDpsId(mapping.dpsCourtAppearanceId)
+    }
+
+  @Transactional
+  suspend fun handleUpsertDuplicateMapping(mapping: CourtScheduleMappingDto, dke: DuplicateKeyException): CourtScheduleMappingUpsertByDpsIdResponse {
+    val existing = getExistingCourtScheduleMappingSimilarTo(mapping)
+    // The duplicate is for an existing DPS ID
+    if (existing.dpsCourtAppearanceId == mapping.dpsCourtAppearanceId) {
+      // The requested NOMIS ID is different so we need to update it
+      if (existing.nomisEventId != mapping.nomisEventId) {
+        updateNomisEventId(mapping.dpsCourtAppearanceId, mapping.nomisEventId, mapping.bookingId)
+        return CourtScheduleMappingUpsertByDpsIdResponse(replacedNomisEventId = existing.nomisEventId)
+      }
+      // We already know about this mapping, so this is a no-op to maintain idempotency
+      else {
+        return CourtScheduleMappingUpsertByDpsIdResponse.EVENT_ID_NOT_REPLACED
+      }
+      // The duplicate is for an existing NOMIS ID but we weren't expecting that, so it appears to be a genuine duplicate
+    } else {
+      throw DuplicateMappingException(
+        messageIn = "Court schedule mapping already exists",
+        duplicate = mapping,
+        existing = existing,
+        cause = dke,
+      )
+    }
+  }
 }
 
 fun CourtScheduleMappingDto.toMapping(): CourtScheduleMapping = CourtScheduleMapping(
