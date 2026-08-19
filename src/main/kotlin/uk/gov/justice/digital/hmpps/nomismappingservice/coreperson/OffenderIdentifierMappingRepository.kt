6c3b407f379e3333f6024331f0de44790e1a8532
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-08-19 17:44:19 +0100

    SDIT-4164: ✨ Add in offender identifier table (#786)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomismappingservice/coreperson/OffenderIdentifierMappingRepository.kt
@@ -0,0 +1,12 @@
+package uk.gov.justice.digital.hmpps.nomismappingservice.coreperson
+
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface OffenderIdentifierMappingRepository : CoroutineCrudRepository<OffenderIdentifierMapping, String> {
+  suspend fun findOneByNomisOffenderIdAndNomisIdentifierSequence(nomisOffenderId: Long, nomisIdentifierSequence: Long): OffenderIdentifierMapping?
+  suspend fun findOneByCprId(cprId: String): OffenderIdentifierMapping?
+  suspend fun deleteByNomisOffenderIdAndNomisIdentifierSequence(nomisOffenderId: Long, nomisIdentifierSequence: Long)
+  suspend fun deleteAllByNomisPrisonNumber(nomisPrisonNumber: String)
+}
